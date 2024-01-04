b1419ed3d69f8409b446e0e460c6772e9d90bc46
Jay McKerracher <jaymckerracher@hotmail.co.uk>
2024-01-04 09:25:09 +0000

    trying to make the test carousel work

--- components/CarouselComponent.js
+++ components/CarouselComponent.js
@@ -1,26 +1,34 @@
 import * as React from 'react';
-import { Dimensions, Text, View, StyleSheet } from 'react-native';
+import { Dimensions, Text, View, StyleSheet, Image } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
 
 export default function CarouselComponent() {
+
     const width = Dimensions.get('window').width;
+    const testImages = [
+        {imageUrl: 'https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_3x2.jpg'},
+        {imageUrl: 'https://d3544la1u8djza.cloudfront.net/APHI/Blog/2021/07-06/small+white+fluffy+dog+smiling+at+the+camera+in+close-up-min.jpg'}
+    ]
+
     return (
-        <View style={{ flex: 1 }}>
-            <Carousel
-                loop
-                width={width}
-                height={width / 2}
-                autoPlay={true}
-                data={[1, 2, 3]}
-                scrollAnimationDuration={1000}
-                onSnapToItem={(index) => console.log('current index:', index)}
-                renderItem={({ index, data }) => (
-                    <View style={styles.component}>
-                        <Text style={styles.componentText}>{data[index]}</Text>
-                    </View>
-                )}
-            />
-        </View>
+        <Carousel
+            // style="stack-vertical-left"
+            loop
+            width={width}
+            height={width / 2}
+            autoPlay={true}
+            data={testImages}
+            scrollAnimationDuration={1000}
+            // onSnapToItem={(index) => console.log('current index:', index)}
+            renderItem={({ imageUrl }) => (
+                <View style={styles.component}>
+                    <Image
+                        source={{uri: imageUrl}}
+                        // style={{flex: 1, resizeMode: 'cover'}}
+                    />
+                </View>
+            )}
+        />
     );
 }
 
